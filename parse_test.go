5057e59b7f0e9df4444ce67908cbcce185419c35
Wilhelm Wijkander <wilhelm.wijkander@0x5e.se>
2024-09-13 11:47:07 +0200

    add .ax testcase

--- parse_test.go
+++ parse_test.go
@@ -26,6 +26,7 @@ func TestParsing(t *testing.T) {
 		{configDomain: ConfigDomain{Domain: "nic.cz", Status: []string{"Sponsoring registrar change forbidden"}, Nameservers: []string{"d.ns.nic.cz (193.29.206.1, 2001:678:1::1)", "b.ns.nic.cz (194.0.13.1, 2001:678:10::1)", "a.ns.nic.cz (194.0.12.1, 2001:678:f::1)"}, Dnssec: "AUTO-ZHHWRIDCGH9F46ZDG2K2VC284", Registrar: "REG-CZNIC"}, date: time.Date(2027, 3, 15, 0, 0, 0, 0, time.UTC)},
 		{configDomain: ConfigDomain{Domain: "maxexperiencia.com.br", Status: []string{"published"}, Nameservers: []string{"a.auto.dns.br", "b.auto.dns.br"}, Dnssec: "33150 ECDSA-SHA-256 5B797E534DEEBAAAD1BDE59F6B06E5AE65580E8E67F8C64152AE6A9A735FF97A"}, date: time.Date(2025, 7, 29, 0, 0, 0, 0, time.UTC)},
 		{configDomain: ConfigDomain{Domain: "foo.rs", Status: []string{"Active"}, Nameservers: []string{"ns1.foodns.net - 91.185.193.152", "ns2.foodns.net - 91.185.193.152"}, Dnssec: "no", Registrar: "Loopia d.o.o."}, date: time.Date(2024, 8, 26, 17, 1, 7, 0, time.UTC)},
+		{configDomain: ConfigDomain{Domain: "alcom.ax", Status: []string{"Registered"}, Nameservers: []string{"ns1.aland.net", "ns3.alcom.fi", "ns2.aland.net", "ns4.alcom.fi"}, Registrar: "Ålands Telekommunikation Ab"}, date: time.Date(2026, 4, 11, 0, 0, 0, 0, time.UTC)},
 	}
 
 	template, err := ioutil.ReadFile("./whois.textfsm")
