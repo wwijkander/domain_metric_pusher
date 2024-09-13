249756300053ef044745be76fee4e7506355e344
Wilhelm Wijkander <wilhelm.wijkander@0x5e.se>
2024-09-13 11:04:59 +0200

    add support for .cz, .br and .rs

--- parse_test.go
+++ parse_test.go
@@ -23,6 +23,9 @@ func TestParsing(t *testing.T) {
 		{configDomain: ConfigDomain{Domain: "yolo.im", Nameservers: []string{"ns-145-a.gandi.net", "ns-191-c.gandi.net", "ns-181-b.gandi.net"}}, date: time.Date(2022, 06, 05, 00, 59, 59, 0, time.UTC)},
 		{configDomain: ConfigDomain{Domain: "com", Status: []string{"OK"}, Registrar: "PDR Ltd. d/b/a PublicDomainRegistry.com"}, date: time.Date(2018, 7, 3, 19, 6, 9, 0, time.UTC)},
 		{configDomain: ConfigDomain{Domain: "io"}, date: time.Date(2018, 12, 21, 17, 35, 22, 0, time.UTC)},
+		{configDomain: ConfigDomain{Domain: "nic.cz", Status: []string{"Sponsoring registrar change forbidden"}, Nameservers: []string{"d.ns.nic.cz (193.29.206.1, 2001:678:1::1)", "b.ns.nic.cz (194.0.13.1, 2001:678:10::1)", "a.ns.nic.cz (194.0.12.1, 2001:678:f::1)"}, Dnssec: "AUTO-ZHHWRIDCGH9F46ZDG2K2VC284", Registrar: "REG-CZNIC"}, date: time.Date(2027, 3, 15, 0, 0, 0, 0, time.UTC)},
+		{configDomain: ConfigDomain{Domain: "maxexperiencia.com.br", Status: []string{"published"}, Nameservers: []string{"a.auto.dns.br", "b.auto.dns.br"}, Dnssec: "33150 ECDSA-SHA-256 5B797E534DEEBAAAD1BDE59F6B06E5AE65580E8E67F8C64152AE6A9A735FF97A"}, date: time.Date(2025, 7, 29, 0, 0, 0, 0, time.UTC)},
+		{configDomain: ConfigDomain{Domain: "foo.rs", Status: []string{"Active"}, Nameservers: []string{"ns1.foodns.net - 91.185.193.152", "ns2.foodns.net - 91.185.193.152"}, Dnssec: "no", Registrar: "Loopia d.o.o."}, date: time.Date(2024, 8, 26, 17, 1, 7, 0, time.UTC)},
 	}
 
 	template, err := ioutil.ReadFile("./whois.textfsm")
---
+++ testdata/foo.rs
@@ -0,0 +1,43 @@
+% The data in the Whois database are provided by RNIDS
+% for information purposes only and to assist persons in obtaining
+% information about or related to a domain name registration record. 
+% Data in the database are created by registrants and we do not guarantee 
+% their accuracy. We reserve the right to remove access 
+% for entities abusing the data, without notice.
+% All timestamps are given in Serbian local time. 
+%
+Domain name: foo.rs
+Domain status: Active https://www.rnids.rs/en/domain-name-status-codes#Active
+Registration date: 26.08.2013 17:01:07
+Modification date: 25.08.2022 11:38:24
+Expiration date: 26.08.2024 17:01:07
+Confirmed: 26.08.2013 17:01:07
+Registrar: Loopia d.o.o.
+
+
+Registrant: Foo Media D.O.O.
+Address: Tošin Bunar 188a, Novi Beograd, Serbia
+Postal Code: 
+ID Number: 20836148
+Tax ID: 107609600
+
+Administrative contact: Foo Media D.O.O.
+Address: Tošin Bunar 188a, Novi Beograd, Serbia
+Postal Code: 
+ID Number: 20836148
+Tax ID: 
+
+Technical contact: Loopia d.o.o.
+Address: Obrenoviceva 46 TPC KALCA C1/72, Nis, Serbia
+Postal Code: 18000
+ID Number: 17503626
+Tax ID: 
+
+
+DNS: ns1.foodns.net - 91.185.193.152
+DNS: ns2.foodns.net - 91.185.193.152
+
+
+DNSSEC signed: no
+
+Whois Timestamp: 19.08.2024 13:46:00
